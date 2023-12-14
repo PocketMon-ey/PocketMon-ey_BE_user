5c8bd20423c12ecb42eef03aeba98600a14104b2
김지훈 <ben224@naver.com>
2023-12-14 15:00:17 +0900

    init project

---
+++ user/src/test/java/com/pocketmoney/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocketmoney.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

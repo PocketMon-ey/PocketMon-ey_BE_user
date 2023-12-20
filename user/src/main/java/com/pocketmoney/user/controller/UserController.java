7a7b876a81fcb2f114c463116e88ad08cd751328
k03147 <ek03147@gmail.com>
2023-12-20 10:54:53 +0900

    Revert "🔨/user 제거"
    This reverts commit c16d5fda59bdf26b2eb280249bf034533dc7b230.

--- user/src/main/java/com/pocketmoney/user/controller/UserController.java
+++ user/src/main/java/com/pocketmoney/user/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-// @RequestMapping("/user")
+@RequestMapping("/user")
 @Api(tags = "User")
 public class UserController {
 

94aa586478cf5d8266e68088e01c4073710ee7bc
k03147 <ek03147@gmail.com>
2023-12-20 10:20:32 +0900

    ✨계좌이체 추가, 오타수정

--- user/src/main/java/com/pocketmoney/user/Service/UserServiceImpl.java
+++ user/src/main/java/com/pocketmoney/user/Service/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.pocketmoney.user.Service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.pocketmoney.user.dao.UserDao;
 import com.pocketmoney.user.model.BalanceParam;
 import com.pocketmoney.user.model.User;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -25,7 +27,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public int updateBalance(BalanceParam balanceParam) throws Exception {
-        return userDao.updateBalance(balanceParam);
+        int id1 = balanceParam.getId1();
+        int id2 = balanceParam.getId2();
+        int price = balanceParam.getPrice();
+        BalanceParam bp = new BalanceParam(id2, id1, -price);
+        if (userDao.updateBalance(bp) == 1){
+            return userDao.updateBalance(balanceParam);
+        }
+        return 0;
     }
 
     @Override

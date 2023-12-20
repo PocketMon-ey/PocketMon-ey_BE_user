36f13d159a81925b266d2352e9faffffaf309629
k03147 <ek03147@gmail.com>
2023-12-20 10:17:35 +0900

    📝Constructor 추가

--- user/src/main/java/com/pocketmoney/user/model/BalanceParam.java
+++ user/src/main/java/com/pocketmoney/user/model/BalanceParam.java
@@ -4,6 +4,11 @@ import lombok.Data;
 
 @Data
 public class BalanceParam {
+    public BalanceParam(int id1, int id2, int price) {
+        this.id1 = id1;
+        this.id2 = id2;
+        this.price = price;
+    }
     private int id1;
     private int id2;
     private int price;
